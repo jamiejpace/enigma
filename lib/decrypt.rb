472e1a013924fe2af6280b6bc55e3a5db63182c8
Jamie Pace <jamiejpace@gmail.com>
2021-08-06 13:26:00 -0600

    Changes to decrypt runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -7,8 +7,11 @@ incoming_text = handle.read
 handle.close
 
 enigma = Enigma.new
-decrypted_message = enigma.decrypt(incoming_text.strip)
+hash = eval(incoming_text)
+decrypted_message = enigma.decrypt(hash[:encryption], ARGV[2], ARGV[3])
+puts "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{
+decrypted_message[:date]}"
 
 writer = File.open(ARGV[1], "w")
-writer.write(encrypted_message)
+writer.write(decrypted_message)
 writer.close
