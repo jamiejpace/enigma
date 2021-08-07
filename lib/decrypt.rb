28d387d3b997789c421673d512df9eaff56b10c8
Jamie Pace <jamiejpace@gmail.com>
2021-08-07 11:19:17 -0600

    Change output of decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -13,5 +13,5 @@ puts "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{
 decrypted_message[:date]}"
 
 writer = File.open(ARGV[1], "w")
-writer.write(decrypted_message)
+writer.write(decrypted_message[:decryption])
 writer.close
